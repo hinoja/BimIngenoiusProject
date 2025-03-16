bccb6421b5db26b7800c0e20c654c84f534d6db8
hinoja <hinoja2@gmail.com>
2025-03-16 16:31:59 +0100

    update views of backoffice

--- resources/views/includes/back/navbar.blade.php
+++ resources/views/includes/back/navbar.blade.php
@@ -1,55 +1,4 @@
 {{-- <nav class="navbar navbar-expand-lg main-navbar">
-    <form class="form-inline mr-auto">
-        <ul class="navbar-nav mr-3">
-            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
-        </ul>
-    </form>
-    <ul class="navbar-nav navbar-right">
-        <div class="dropdown d-inline">
-            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-globe"></i>
-                @if (app()->getLocale() === 'fr')
-                    Fr
-                @else
-                    En
-                @endif
-            </button>
-            <div class="dropdown-menu">
-                {{-- <a class="dropdown-item has-icon" href="{{ route('lang', 'fr') }}"> Fr</a>
-              <a class="dropdown-item has-icon" href="{{ route('lang', 'en') }}"> En</a> --}}
-            {{-- </div>
-        </div>
-        <li class="dropdown">
-            <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                <img alt="image" src="{{ asset('assets/back/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">{{ auth()->user()->name }}</div>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right">
-
-                <a href="/" class="dropdown-item has-icon">
-                    <i class="fas fa-home"></i> @lang('Back to home')
-                </a>
-                <a href="#" class="dropdown-item has-icon">
-                    <i class="far fa-user"></i> @lang('Profile')
-                </a>
-                <div class="dropdown-divider"></div>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                        this.closest('form').submit();"
-                        class="dropdown-item has-icon text-danger">
-                        <i class="fas fa-sign-out-alt"></i> @lang('Log Out')
-                    </a>
-                </form>
-            </div>
-        </li>
-    </ul>
-</nav>
-
-  --}}
-  <nav class="navbar navbar-expand-lg main-navbar">
     <div class="container-fluid">
         <!-- Sidebar Toggle -->
         <ul class="navbar-nav mr-3">
@@ -67,7 +16,8 @@
         </a>
 
         <!-- Toggler for Small Screens -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
         </button>
 
@@ -76,7 +26,8 @@
             <ul class="navbar-nav ml-auto">
                 <!-- Language Toggle Dropdown -->
                 <li class="nav-item dropdown d-inline mr-3">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="languageDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="languageDropdown"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fas fa-globe"></i>
                         @if (app()->getLocale() === 'fr')
                             Fr
@@ -93,7 +44,8 @@
                 <!-- User Dropdown -->
                 <li class="nav-item dropdown">
                     <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                        <img alt="image" src="{{ asset('assets/back/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+                        <img alt="image" src="{{ asset('assets/back/img/avatar/avatar-1.png') }}"
+                            class="rounded-circle mr-1">
                         <div class="d-sm-none d-lg-inline-block">{{ auth()->user()->name }}</div>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right">
@@ -107,8 +59,8 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <a href="{{ route('logout') }}"
-                               onclick="event.preventDefault(); this.closest('form').submit();"
-                               class="dropdown-item has-icon text-danger">
+                                onclick="event.preventDefault(); this.closest('form').submit();"
+                                class="dropdown-item has-icon text-danger">
                                 <i class="fas fa-sign-out-alt"></i> @lang('Log Out')
                             </a>
                         </form>
@@ -117,4 +69,51 @@
             </ul>
         </div>
     </div>
+</nav> --}}
+
+
+{{--  --}}
+<nav class="navbar navbar-expand-lg main-navbar">
+    <form class="form-inline mr-auto">
+      <ul class="navbar-nav mr-3">
+        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+      </ul>
+    </form>
+    <ul class="navbar-nav navbar-right">
+        <div class="dropdown d-inline">
+            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-globe"></i>
+                @if (app()->getLocale() === 'fr') Fr @else En @endif
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item has-icon" href="{{ route('lang', 'fr') }}"> Fr</a>
+              <a class="dropdown-item has-icon" href="{{ route('lang', 'en') }}"> En</a>
+            </div>
+          </div>
+        <li class="dropdown">
+            <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="{{ asset('assets/back/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">{{ auth()->user()->name }}</div>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                {{-- <div class="dropdown-title">Logged in 5 min ago</div> --}}
+                <a href="/" class="dropdown-item has-icon">
+                    <i class="fas fa-home"></i> @lang('Back to home')
+                </a>
+                <a href="{{ route('profile.edit') }}" class="dropdown-item has-icon">
+                    <i class="far fa-user"></i> @lang('Profile')
+                </a>
+                <div class="dropdown-divider"></div>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        this.closest('form').submit();"
+                        class="dropdown-item has-icon text-danger">
+                        <i class="fas fa-sign-out-alt"></i> @lang('Log Out')
+                    </a>
+                </form>
+            </div>
+        </li>
+    </ul>
 </nav>
--- resources/views/includes/back/sidebar.blade.php
+++ resources/views/includes/back/sidebar.blade.php
@@ -1,15 +1,19 @@
 <!-- Sidebar Section -->
+
+@php
+    $currentUri = Route::current()->uri;
+@endphp
+
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <br>
-            <a href="#" class="d-flex align-items-center">
-                {{-- <img alt="image" src="{{ asset('logoSvg.svg') }}" class="mr-2" width="60" style="border-radius: 12px;"> --}}
-                <span class="sidebar-brand-name">{{ config('app.name', 'BIM INGENIOUS BTP') }}</span>
+            <a href="{{ route('admin.dashboard') }}">
+                <img alt="image" src="{{ asset('assets/logo.png') }}" class="rounded-circle mr-1" width="60">
+                {{ config('app.name', 'MboaLink') }}
             </a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="#">BI BTP</a>
+            <a href="{{ route('admin.dashboard') }}">Mb</a>
         </div>
         <ul class="sidebar-menu">
             <li class="@if (Str::contains(Route::current()->uri, 'dashboard')) active @endif">
@@ -17,9 +21,11 @@
                     <span>@lang('Dashboard')</span></a>
             </li>
             <li class="@if (Str::contains(Route::current()->uri, 'users')) active @endif">
-                <a class="nav-link" href="#"><i class="fas fa-users"></i>
+                <a class="nav-link" href="{{ route('admin.users.index') }}"><i class="fas fa-users"></i>
                     <span>@lang('Users')</span></a>
             </li>
+
+
         </ul>
     </aside>
 </div>
