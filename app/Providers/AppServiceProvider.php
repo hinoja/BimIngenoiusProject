040e7500c27a12e4a0ab051eada2840d4e5615ec
hinoja <hinoja2@gmail.com>
2025-07-07 00:44:24 +0200

    Add default string length configuration in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
@@ -17,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+         Schema::defaultStringLength(191);
         // Add any boot-time registration here
     }
 }
